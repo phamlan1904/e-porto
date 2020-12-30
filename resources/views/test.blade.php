d4e714e018e56f8705ff76dff0c79026e98d8f2b
plvnjp1904 <pl.vnjp@gmail.com>
2020-12-30 14:27:46 +0900

    virtual setup

---
+++ resources/views/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="text-danger">test text fgdfsgdfsg</div>
+</body>
+</html>
+
